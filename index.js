871a07c34a17220f577d4e90dea1332f793cc40e
paito0909 <paito.naranjoq@gmail.com>
2022-09-24 22:00:41 -0500

    cuarto commit

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-let listaProductos = [];
+//Crear un nuevo producto
+document.getElementById("formulario").addEventListener("submit",crear);
 
-let editar = false;
+//Función crear
+function crear(e){
+    nombre = document.getElementById("product_name").value
+    precio = document.getElementById("product_price").value
+    descripcion = document.getElementById("product_detail").value
 
-const form = document.
+    let producto = {
+        nombre,
+        precio,
+        descripcion
+    }
 
-
-
+    if localStorage.getItem("Productos") === Null){
+        let productos =[]
+        productos.push(producto)
+        localStorage.setItem("Productos",JSON.stringify(productos))
+    }else{
+        let productos = localStorage.getItem(JSON.parase("Productos"))
+        productos.push(producto)
+        localStorage.getItem("Productos",JSON.stringify(productos))
+    }
+    document.getElementById("form").reset();
+    console.log("Producto crado correctamente")
+    e.preventDefault();
+}
